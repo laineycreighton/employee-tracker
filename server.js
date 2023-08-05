753225187e802d6d2b0b05a9411c9a9269bed2dc
Lainey Creighton <dev.lainey@gmail.com>
2023-08-04 23:25:36 -0400

    add pseudocode for server functions

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const mysql = require('mysql2')
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -15,4 +17,30 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the the employee_db database.')
 );
+
+const viewAllDepartments = () => {
+//retrieve departments here
+};
+
+const viewAllRoles = () => {
+    //retrieve roles here
+};
+
+const viewAllEmployees = () => {
+    //retrieve employees here
+};
+
+const addRole = () => {
+    //prompt user for role details
+    //add and execute a role and connection to the database
+};
+
+const addEmployee = () => {
+    //prompt user for employee details
+    //add and execute an employee and connection to the database
+};
+
+const updateEmployeeRole = () => {
+    //prompt user for the 
+}
 

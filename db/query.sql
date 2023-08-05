1cd212a25e43a36c34be74ee5fcf56ee5d1d2416
Lainey Creighton <dev.lainey@gmail.com>
2023-08-04 23:16:44 -0400

    add queries for the database

--- db/query.sql
+++ db/query.sql
@@ -1,2 +1,14 @@
-SELECT departments.name AS department, role.title, employee.role_id
-FROM 
+SELECT id AS department_id, name AS department_name
+FROM departments;
+
+SELECT role.id AS role_id, role.title AS role_title, role.salary, departments.name AS department_name
+FROM role
+INNER JOIN departments ON role.department_id = departments.id;
+
+SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS job_title, 
+       departments.name AS department_name, role.salary, 
+       CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name
+FROM employee
+INNER JOIN role ON employee.role_id = role.id
+INNER JOIN departments ON role.department_id = departments.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id;

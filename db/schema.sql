753d56e66cc41d1e311330871dc362b2cc615abc
Lainey Creighton <dev.lainey@gmail.com>
2023-08-04 23:02:08 -0400

    add seeds to database

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,28 @@
-INSERT INTO employees (employee_name)
-VALUES ()
+INSERT INTO
+    department (name)
+VALUES
+    ("Executive"),
+    ("HR"),
+    ("Finance"),
+    ("Operations"),
+    ("Sales");
+
+INSERT INTO
+    role (title, salary, department_id)
+VALUES
+    ("Associate", 75000.00, (2, 3, 4, 5)),
+    ("Supervisor", 100000.00, (2, 3, 4, 5)),
+    ("Manager", 125000.00, (2, 3, 4, 5)),
+    ("District Manager", 175000.00, 1),
+    ("Vice President", 250000.00, 1),
+    ("CEO", 350000.00, 1);
+
+INSERT INTO
+    employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Lainey", "Creighton", 6),
+    ("Ashley", "Huskey", 5, 6),
+    ("Salmon", "Creighton", 4, 5),
+    ("Tilapia", "Creighton", 3, 4),
+    ("Amora", "Huskey", 2, 3),
+    ("Lyla", "Aldridge", 1, 2);
